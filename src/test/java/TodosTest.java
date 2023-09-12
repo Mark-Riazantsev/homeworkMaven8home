e6d25f0f1ab861e8934f8720e9f96413bd06d7f0
Riazantsev <riazantsevmark327@gmail.com>
2023-09-12 08:37:44 +0400

    almost done

---
+++ src/test/java/TodosTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodosTest {
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = { simpleTask, epic, meeting };
+        Task[] actual = todos.findAll();
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void falsetestSearch() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        Epic epic = new Epic(55, new String[]{"Молоко", "Яйца", "Хлеб"});
+        Meeting meeting = new Meeting(555, "Выкатка 3й версии приложения", "Приложение НетоБанка", "Во вторник после обеда");
+
+        Task[] tasks = {simpleTask, epic, meeting};
+        Todos todos = new Todos(tasks);
+
+        Task[] expected = {epic, meeting};
+        Task[] result = todos.search("Выкатка");
+
+        assertArrayEquals(expected, result);
+    }
+}
