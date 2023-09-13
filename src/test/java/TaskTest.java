3a85ef3d7e923d9daca7932f9114556aa1484211
Riazantsev <riazantsevmark327@gmail.com>
2023-09-13 07:09:09 +0400

    Build succes

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class TaskTest {
 @Test
 public void falseMatchesSimpleTask() {
--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -30,15 +30,39 @@ public class TodosTest {
         assertArrayEquals(expected, actual);
     }
     @Test
-    public void falsetestSearch() {
+    public void falseTestSearch() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        SimpleTask simpleTask1 = new SimpleTask(6, "лягти спати");
         Epic epic = new Epic(55, new String[]{"Молоко", "Яйца", "Хлеб"});
         Meeting meeting = new Meeting(555, "Выкатка 3й версии приложения", "Приложение НетоБанка", "Во вторник после обеда");
 
-        Task[] tasks = {simpleTask, epic, meeting};
-        Todos todos = new Todos(tasks);
 
-        Task[] expected = {epic, meeting};
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+        todos.add(simpleTask1);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = new Task[0];
+        Task[] result = todos.search("Закрытие");
+
+        assertArrayEquals(expected, result);
+    }
+    @Test
+    public void trueTestSearch() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        Epic epic = new Epic(55, new String[]{"Молоко", "Яйца", "Выкатка"});
+        Meeting meeting = new Meeting(555, "Выкатка 3й версии приложения", "Приложение НетоБанка", "Во вторник после обеда");
+
+
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = new Task[2];
+        expected[0] = epic;
+        expected[1] = meeting;
         Task[] result = todos.search("Выкатка");
 
         assertArrayEquals(expected, result);
